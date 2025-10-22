06af3d376123490b1ac71d24a3679e2c39b41c35
Badhan Das <badhan.2192@gmail.com>
2025-10-22 18:25:42 -0400

    Refactor script to use scripts directory for Python files

--- baseline_models.sh
+++ baseline_models.sh
@@ -1,63 +1,44 @@
 #!/bin/bash
-#SBATCH --job-name=habijabi
-#SBATCH --account=gpce
-#SBATCH --partition=a100_normal_q
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --time=20:00:00
-#SBATCH --gres=gpu:1        # Request 1 GPU
-#SBATCH --mem=32G           # Request 32 GB of memory
-#SBATCH --cpus-per-task=16
-#SBATCH --mail-type=ALL   # Send t notification at the start and end of the job
-#SBATCH --mail-user=badhan@vt.edu   # Send email notification to this address
-#SBATCH --output=/home/badhan/quasi/slurm_out/%j.out
-
-
+#add your slurm spec here
 
 echo $1
 dir="$1"   #directory
 
 #node feature with pca
-python3 pca_node_features.py "$dir"
+python3 scripts/pca_node_features.py "$dir"
 
 #extract valid indices
-python3 extract_valid_indices.py "$dir"
+python3 scripts/extract_valid_indices.py "$dir"
 
 #GCN
-python3 GCN.py "$dir"
+python3 scripts/GCN.py "$dir"
 
 #graphSAGE node prediction
-python3 graphSAGE_node_pred.py "$dir"
+python3 scripts/graphSAGE_node_pred.py "$dir"
 
-python3 edge_index_label.py "$dir"
+python3 scripts/edge_index_label.py "$dir"
 
 #graphSAGE edge prediction
-python3 graphSAGE_edge_pred.py "$dir"
+python3 scripts/graphSAGE_edge_pred.py "$dir"
 
 #GAT node prediction
-python3 GAT_node_pred.py "$dir"
+python3 scripts/GAT_node_pred.py "$dir"
 
 #GAT edge prediction
-python3 GAT_edge_pred.py "$dir"
+python3 scripts/GAT_edge_pred.py "$dir"
 
 #VGAE edge prediction
-python3 VGAE_edge_pred.py "$dir"
+python3 scripts/VGAE_edge_pred.py "$dir"
 
 #GGNN node prediction
-python3 GGNN_node_pred.py "$dir"
+python3 scripts/GGNN_node_pred.py "$dir"
 
 #GGNN edge prediction
-python3 GGNN_edge_pred.py "$dir"
+python3 scripts/GGNN_edge_pred.py "$dir"
 
 #MLP node prediction
-python3 MLP.py "$dir"
+python3 scripts/MLP.py "$dir"
 
 #MLP edge prediction
-python3 MLP_edge_pred.py "$dir"
-
-end=$(date +%s%3N)  # End time in milliseconds
-elapsed=$((end - start))
-
-echo "Elapsed time: $elapsed milliseconds"
-
+python3 scripts/MLP_edge_pred.py "$dir"
 echo "job $SLURM_JOB_ID has ended on node"
